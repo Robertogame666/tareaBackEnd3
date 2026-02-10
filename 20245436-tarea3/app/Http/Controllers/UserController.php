d49818c570b1e67cecee9865678dd44e75132486
Robertogame666 <88417662+Robertogame666@users.noreply.github.com>
2026-02-09 19:28:25 -0600

    Code

---
+++ 20245436-tarea3/app/Http/Controllers/UserController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Resources\UserResource;
+use App\Http\Requests\UpdateUserRequest;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+    if ($request->boolean('trashed')) {
+        $users = User::onlyTrashed()->get();
+    } else {
+        $users = User::all();
+    }
+
+    return UserResource::collection($users);
+}
+
+    public function show($id)
+    {
+    $user = User::findOrFail($id);
+
+    return new UserResource($user);
+    }
+
+    public function destroy($id)
+    {
+    $user = User::findOrFail($id);
+
+    $user->delete();
+
+    return response()->json([
+        'message' => 'El usuario ha sido eliminado correctamente.'
+    ], 200);
+    }
+
+    public function update(UpdateUserRequest $request, $id)
+    {
+    $user = User::findOrFail($id);
+
+    $user->update($request->validated());
+
+    return response()->json(
+        new UserResource($user),
+        200
+    );
+    }
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+    $data = $request->validated();
+
+    $data['password'] = Str::random(8);
+    $data['hiring_date'] = $data['hiring_date'] ?? now();
+
+    $user = User::create($data);
+
+    return response()->json(
+        new UserResource($user),
+        201
+    );
+    }
+}
---
+++ 20245436-tarea3/app/Models/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class User extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'username',
+        'dui',
+        'phone_number',
+    ];
+}
